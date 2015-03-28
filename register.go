2150c3e2f2130d6708210f3a68a1c5c87b37fb8f
icereval <icereval@gmail.com>
2015-03-28 17:56:19 -0400

    v1beta3 api

--- register.go
+++ register.go
@@ -5,30 +5,39 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-        "io/ioutil"
+  "io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Minion struct {
+type Node struct {
 	Kind   string `json:"kind,omitempty"`
-	ID     string `json:"id,omitempty"`
-	HostIP string `json:"hostIP,omitempty"`
-        APIVersion string `json:"apiVersion,omitempty"`
+	UID     string `json:"uid,omitempty"`
+  APIVersion string `json:"apiVersion,omitempty"`
+}
+
+type NodeStatus struct {
+  HostIP string `json:"hostIP,omitempty"`
+}
+
+type NodeResp struct {
+  Reason string `json:"reason,omitempty"`
 }
 
 func register(endpoint, addr string) error {
-	m := &Minion{
-		Kind:   "Minion",
-                APIVersion: "v1beta1",
-		ID:     addr,
-		HostIP: addr,
+	node := &Node{
+		Kind: "Node",
+    APIVersion: "v1beta3",
+		UID: addr,
+		&NodeStatus{
+			HostIP: addr,
+		},
 	}
-	data, err := json.Marshal(m)
+	data, err := json.Marshal(node)
 	if err != nil {
 		return err
 	}
-	url := fmt.Sprintf("%s/api/v1beta1/minions", endpoint)
+	url := fmt.Sprintf("%s/api/v1beta3/nodes", endpoint)
 	res, err := http.Post(url, "application/json", bytes.NewReader(data))
 	if err != nil {
 		return err
@@ -38,8 +47,19 @@ func register(endpoint, addr string) error {
 		log.Printf("registered machine: %s\n", addr)
 		return nil
 	}
-        data, err = ioutil.ReadAll(res.Body)
-        log.Printf("Response: %#v", res)
-        log.Printf("Response Body:\n%s", string(data))
-	return errors.New("error registering: " + addr)
+	nodeResp := &NodeResp{}
+	data, err = ioutil.ReadAll(res.Body)
+	json.Unmarshal([]byte(data), &nodeResp)
+	if res.StatusCode == 409 && nodeResp.Reason == "AlreadyExists" {
+		log.Printf("Already registered machine: %s\n", addr)
+		return nil
+	}
+	log.Printf("Response: %#v", res)
+	log.Printf("Response Body:\n%s", string(data))
+	body, err := ioutil.ReadAll(res.Body)
+	reason := ""
+	if err == nil {
+		reason = ": " + string(body)
+	}
+	return errors.New("error registering: " + addr + reason)
 }
